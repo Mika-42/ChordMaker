760df3c40c7005679137de9d7e4a550ccb126990
Mika <matis.mikael@gmail.com>
2024-02-04 16:19:35 +0100

    ui

--- main.py
+++ main.py
@@ -3,14 +3,93 @@
 #
 #   author: matis
 # ------------------------------------------------------------------------------
-from tkinter import *
-from chordProgression import *
+import kivy
+kivy.require('2.1.0') 
 
-def main() -> int:
-    window = Tk()
-    #c = ChordProgression('C', 'maj', 8)
-    #print(c)
-    return 0
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '450')
+
+from kivymd.app import MDApp
+from kivy.core.text import LabelBase
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+
+class ChordButton(MDFlatButton, MDToggleButton):
+    pass
+
+class ScaleSelector(MDBoxLayout):
+    pass
+   
+class RootNoteLabel(MDLabel):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.currentNoteIndex = 0
+        self.notes = ['C', 'C#', 'D', 'D#',
+                       'E', 'F', 'F#', 'G',
+                       'G#', 'A', 'A#', 'B']
+        
+    def on_touch_down(self, touch):
+
+        if touch.is_mouse_scrolling and self.collide_point(*touch.pos):
+            if touch.button == 'scrollup':
+                self.currentNoteIndex -= 1
+
+                if self.currentNoteIndex < 0:
+                    self.currentNoteIndex = 11
+
+            elif touch.button == 'scrolldown':
+                self.currentNoteIndex += 1
+
+                if self.currentNoteIndex > 11:
+                    self.currentNoteIndex = 0
+
+        self.text = self.notes[self.currentNoteIndex]
+
+        super().on_touch_down(touch)
+    
+class ModeLabel(MDLabel):
+   
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.currentModeIndex = 0
+        self.mode = ('maj', 'min')
+        
+    def on_touch_down(self, touch):
+
+        if touch.is_mouse_scrolling and self.collide_point(*touch.pos):
+            if touch.button in ('scrollup', 'scrolldown'):
+                    self.currentModeIndex = not self.currentModeIndex
+
+        self.text = self.mode[self.currentModeIndex]
+
+        super().on_touch_down(touch)
+     
+class MainView(MDScreen):
+    
+    def disableButton(self, btn):
+        _btn = self.ids[f'btn{btn}']
+        _prev_btn = self.ids[f'btn{btn - (btn > 0)}']
+        _next_btn = self.ids[f'btn{btn + (btn < 7)}']
+
+        if _btn.state == 'down':
+            if _prev_btn.state != 'down':
+                _btn.state = 'normal'    
+
+        if _btn.state == 'normal':
+            if _next_btn.state == 'down':
+                _btn.state = 'down' 
+
+class ChordMaker(MDApp):
+
+    def builder(self):
+        return MainView()
 
 if __name__ == '__main__':
-    main()
+    LabelBase.register(name='Cabin Sketch', fn_regular='src/CabinSketch.ttf')
+    ChordMaker().run()
